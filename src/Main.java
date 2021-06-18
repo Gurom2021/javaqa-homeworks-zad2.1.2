7433477f272021c4548c89ae78354915528ff448
Gurom2021 <gurom242@gmail.com>
2021-06-18 18:08:56 +0300

    int bonus

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Текущий баланс счета: 100 руб");
-
-        Scanner sc = new Scanner(System.in);
-
+        int balans;
         int refill;
         int bonus;
         int amount;
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Введите началный баланс счета");
+        balans = sc.nextInt();//Считываем начальный баланс
+
         System.out.println("Введите сумму пополнения");
         refill = sc.nextInt();//Считываем сумму пополнения
+
         if (refill > 1000) {
             bonus = refill / 100;
         } else {
             bonus = 0;
         }
-        amount = refill + bonus + 100;
-        System.out.println("Всего бонусных рублей:" + bonus);
-        System.out.println("Баланс счета после поплнения:" + amount);
+
+        amount = balans + refill + bonus;
+        System.out.println("Всего бонусных рублей: " + bonus);
+        System.out.println("Баланс счета после поплнения: " + amount + " рублей");
 
 
     }
